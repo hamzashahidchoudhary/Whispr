479982ae86e8eb4e088de21afb205017600204d1
Muhammad Hamza <hamzachoudhary9454@gmail.com>
2026-07-01 10:08:07 +0500

    Fix

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -5,7 +5,6 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0, viewport-fit=cover">
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <meta name="theme-color" content="#111318">
-    <meta name="apple-mobile-web-app-capable" content="yes">
     <title>Whispr</title>
     @viteReactRefresh
     @vite(['resources/css/app.css', 'resources/js/App.jsx'])
